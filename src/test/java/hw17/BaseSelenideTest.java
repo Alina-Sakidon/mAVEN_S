206a32f4c1f33c0a7c7682a61f76f054b0de4f64
Alina-Sakidon <alinasakidon@gmail.com>
2025-11-25 17:42:07 +0200

    add BaseSelenideTest.java headless mode

--- src/test/java/hw17/BaseSelenideTest.java
+++ src/test/java/hw17/BaseSelenideTest.java
@@ -1,11 +1,18 @@
 package hw17;
 
+import com.codeborne.selenide.Configuration;
 import pages.selenidePages.MainPageSelenide;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class BaseSelenideTest {
-    public MainPageSelenide openMainPage(){
+    static {
+        // Настройки Selenide
+        Configuration.browser = "chrome";
+        Configuration.headless = true;
+    }
+
+    public MainPageSelenide openMainPage() {
         open("https://the-internet.herokuapp.com");
         return new MainPageSelenide();
     }
