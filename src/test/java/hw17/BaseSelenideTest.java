e076b062308d2ac82921fb5392748e38d383ff28
Alina-Sakidon <alinasakidon@gmail.com>
2025-12-01 17:42:55 +0200

    add changes BaseSelenideTest.java

--- src/test/java/hw17/BaseSelenideTest.java
+++ src/test/java/hw17/BaseSelenideTest.java
@@ -1,8 +1,13 @@
 package hw17;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
 import listeners.AllureListener;
+import org.openqa.selenium.OutputType;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Listeners;
 import pages.selenidePages.MainPageSelenide;
 
@@ -13,6 +18,18 @@ public class BaseSelenideTest {
     static {
         Configuration.browser = "chrome";
         Configuration.browserSize = "1920x1080";
+    }
+
+    @AfterMethod
+    public void takeScreenshot(ITestResult result){
+        if(!result.isSuccess()){
+            attachScreenshot("Failure screenshot");
+        }
+    }
+    @Attachment(value = "{name}", type = "image/png")
+    public byte[] attachScreenshot(String name) {
+        // Возвращаем скриншот как массив байт для Allure
+        return Selenide.screenshot(OutputType.BYTES);
     }
 
     @Step("Open main page")
